2b677a7a2af0b289544eceebe95e9240c35cb2be
Meghal Bothra <meghalbothra@gmail.com>
2025-01-15 18:41:45 +0530

    17th commit

--- backend/main.py
+++ backend/main.py
@@ -33,7 +33,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"]  # Allow all HTTP headers
